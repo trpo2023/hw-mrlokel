b142b9dd3bf0a8a3e8c261305fcf60f04470fa60
Dmitriy Oleynikov <is241s13@awp10.csc.local>
2023-02-07 13:35:52 +0700

    Create file

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main()
 {
+  printf("Сумма двух чисел!\n");
+  printf("Введите первое число: ");
+  int a, b;
+  scanf("%d", &a);
+  printf("Введите второе число: ");
+  scanf("%d", &b);
     printf("Hello, World");
     return 0;
 }
