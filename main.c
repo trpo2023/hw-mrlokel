b3bfacd8dd972a3636a2cfb7219134d4d9d1cc10
Dmitriy Oleynikov <is241s13@awp10.csc.local>
2023-02-07 13:56:33 +0700

    Create file

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+C #include<stdio.h>
 
-int main()
+        int main()
 {
-  printf("Сумма двух чисел!\n");
-  printf("Введите первое число: ");
-  int a, b;
-  scanf("%d", &a);
-  printf("Введите второе число: ");
-  scanf("%d", &b);
+    printf("Сумма двух чисел!\n");
+    printf("Введите первое число: ");
+    int a, b;
+    scanf("%d", &a);
+    printf("Введите второе число: ");
+    scanf("%d", &b);
     printf("Hello, World");
     return 0;
 }
